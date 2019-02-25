186889cbd1c133aa1d14273b7cefdf3a6d138b1b
Jeremy Owens <jeremyowens@Jeremys-MacBook-Pro-2.local>
2019-02-25 10:41:49 -0500

    got the front-end loaded and the app an dfile structure set

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import Home from './components/home/Home'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Sanity Check</h1>
+      <div className="container">
+        <Home/>
       </div>
     );
   }
