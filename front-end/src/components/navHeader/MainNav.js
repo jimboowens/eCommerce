210ce7c70c3bf7f42732322c5b60633cc1cf4c1a
Jeremy Owens <jeremyowens@Jeremys-MacBook-Pro-2.local>
2019-02-26 09:49:38 -0500

    updating config file

--- front-end/src/components/navHeader/MainNav.js
+++ front-end/src/components/navHeader/MainNav.js
@@ -6,8 +6,8 @@ function MainNav(props){
             <nav>
                 <div className="nav-wrapper grey darken-4">
                     <ul className="left hide-on-med-and-down">
-                        <li><a href="sass.html">Sass</a></li>
-                        <li><a href="badges.html">Components</a></li>
+                        <li className="active"><a href="sass.html">Sass</a></li>
+                        <li className="active"><a href="badges.html">Components</a></li>
                         <li className="active"><a href="collapsible.html">JavaScript</a></li>
                     </ul>
                 </div>
