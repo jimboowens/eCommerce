b6485301c6de8b592fb10892b8f755bb2501ad99
Jeremy Owens <jeremyowens@Jeremys-MacBook-Pro-2.local>
2019-02-26 11:13:27 -0500

    working throught github authorization

--- back-end/app.js
+++ back-end/app.js
@@ -1,12 +1,40 @@
 var express = require('express');
 var path = require('path');
+var app = express();
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var GitHubStrategy = require('passport-github').Strategy;
+const config = require('./config')
+const helmet = require('helmet')
+app.use(helmet());
+
+// Allow cross-origin.....
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
+
+//+++++++++++++++++PASSPORT FILES++++++++++++++++//
+const passport = require('passport')
+
+
+passport.use(new GitHubStrategy({
+    clientID: config.passport.clientID,
+    clientSecret: config.passport.clientSecret,
+    callbackURL: config.passport.callbackURL,
+  },
+  function(accessToken, refreshToken, profile, callback) {
+    console.log('function ran',profile)
+  }
+));
+
+
+
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,6 +43,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 module.exports = app;
--- front-end/src/components/navHeader/LoginNavBar.js
+++ front-end/src/components/navHeader/LoginNavBar.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import './Nav.css'
+import loginTab from '../../misc/OpenWindow';
+ // The process: (will be enumerated)
 
-function LoginNavBar(props){
-    // The process:
-            // 1. User clicks and opens the new window via loginTab
-            // 2. New window is open to crossOrigin but is github.com
+class LoginNavBar extends Component{
+    constructor(){
+        super()
+    }
+
+    githubAuth = (event)=>{
+        // 1. User clicks and opens the new window via loginTab
+        // 2. New window is open to crossOrigin but is github.com
+        loginTab(`http://localhost:3000/auth/github`)
+    }
+         
             // 3. Once user authenticates, github sends them to /auth/github/callback
             // 4. The callback URL either gets the uid or inserts them
             // 5. Callback then takes the uid and tokenizes it with JWT
@@ -13,14 +22,17 @@ function LoginNavBar(props){
                 // sends the data back over to original page
             // 7. It's now available in this promise resolution
             // 8. Put it in localstorage so we can use it next time.
-    return(
+
+    render(){
+        return(
         <div className="login-nav-bar">
             <div className="left valign-wrapper">WELCOME TO GAME WORLD</div>
             <div className="right">
-                <button type="submit" class="btn play-button btn-github waves-effect grey darken-2">Login with github</button>
-                <button type="submit" class="btn play-button waves-effect grey darken-2">MY CART 0 ITEM - $0.00</button>
+                <button type="button" onClick={this.githubAuth}   class="btn play-button btn-github waves-effect grey darken-2">Login with github</button>
+                <button type="submit" class="btn play-button waves-effect grey darken-2" href="cart">MY CART 0 ITEM - $0.00</button>
             </div>
         </div>
-    )
+        )
+    }
 }
 export default LoginNavBar;
